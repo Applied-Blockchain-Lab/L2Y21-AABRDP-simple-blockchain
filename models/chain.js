f9726554fe66537a6b406427c983bf8b660b363a
Desi <desislava2810@abv.bg>
2021-12-09 13:52:40 +0200

    feat: Add transaction class

--- models/chain.js
+++ models/chain.js
@@ -1,20 +1,43 @@
 const Block = require('./block');
-
+const Transaction = require('./transaction');
+const { BLOCK_SIZE } = require('.././network-parameters');
 class Chain {
 
     constructor() {
         this.chain = [Block.genesisBlock()];
+        this.pendingTransactions = [];
     }
 
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
+    addTransaction(fromAddress, toAddress, amount, myKeyForSign) {
+        if(fromAddress==='' || toAddress==='')
+            return ('Transaction must include from and to addresses!');
+
+        let transaction = new Transaction(fromAddress, toAddress, amount, myKeyForSign);
+        if(!transaction.isValid())
+            return ('Cannot add invalid transaction!');
+
+        this.pendingTransactions.push(transaction);
     }
 
-    addBlock(data) {
-        const newBlock = Block.mineBlock(this.getLatestBlock(), data);
+    addBlock() {
+        if(this.pendingTransactions.length === 0)
+            return ('No pendingTransactions - cannot mine new block!');
+
+        const newBlock = Block.mineBlock(this.getLatestBlock(), this.getFromPendingTransactions());
         this.chain.push(newBlock);
 
         return newBlock;
+    }
+
+    replaceChain(newChain) {
+        if(newChain.length <= this.chain.length)
+            return 'Received chain in not longer than the current chain.';
+
+        if (!this.isValidChain(newChain))
+            return 'Received chain is not valid.';
+
+        this.chain = newChain;
+            return 'Replacing with the new chain!';
     }
 
     isValidChain() {
@@ -22,27 +45,40 @@ class Chain {
             return false;
 
         for(let i = 1; i < this.chain.length; i++) {
-            const block = this.chain[i];
-            const lastBlock = this.chain[i-1];
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
 
-            if(block.parentHash !== lastBlock.hash || block.hash !== Block.blockHash(block))
-            return false;
+            if(currentBlock.parentHash !== previousBlock.hash || currentBlock.hash !== Block.blockHash(currentBlock))
+                return false;
 
+            if(!currentBlock.hasValidTransactions())
+                return false;
         }
         return true;
     }
 
-    replaceChain(newChain) {
-        if(newChain.length <= this.chain.length) {
-            console.log('Received chain in not longer than the current chain.');
-            return;
-        } else if (!this.isValidChain(newChain)) {
-            console.log('Received chain is not valid.');
-            return;
-        }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
 
-        this.chain = newChain;
-        console.log('Replacing with the new chain!')
+    getFromPendingTransactions() {
+        return this.pendingTransactions[BLOCK_SIZE-1] ? this.pendingTransactions.splice(0, BLOCK_SIZE) : this.pendingTransactions.splice(0, this.pendingTransactions.length);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for(const block of this.chain) {
+            for(const transaction of block.transactions) {
+                if(transaction.fromAddress === address) {
+                    balance -= transaction.amount;
+                }
+                if(transaction.toAddress === address) {
+                    balance += transaction.amount;
+                }
+            }
+        }
+        return balance;
     }
 }
 module.exports = Chain;
