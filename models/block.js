fabfcf8a89f29580f3d525d6bc37946aa428a953
cr0c0pk1 <alpics1337@gmail.com>
2021-12-09 16:45:18 +0200

    test: Test husky pre-commit hook

--- models/block.js
+++ models/block.js
@@ -58,7 +58,7 @@ class Block {
             if (!tx.isValid()) {
                 return false;
             }
-            return true;
+            return true;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         });
     }
 }
