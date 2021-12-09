9c69d035aa57c9b67ae62d5537d3fb84744637e2
cr0c0pk1 <alpics1337@gmail.com>
2021-12-09 17:19:47 +0200

    feat: Add ESLint AirBnb style guide and husky pre-commit hook

--- models/block.js
+++ models/block.js
@@ -58,7 +58,7 @@ class Block {
             if (!tx.isValid()) {
                 return false;
             }
-            return true; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+            return true;
         });
     }
 }
