33d192e1d1ca888a55fd62be2bd00c6910fe6a6b
Desi <desislava2810@abv.bg>
2021-12-14 18:19:37 +0200

    fix: Fix api.test.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+
     },
     rules: {
         'linebreak-style': 0,
--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,96 +1,85 @@
-const server = require('../app');
 const axios = require('axios');
 const jestOpenAPI = require('jest-openapi').default;
 const Generator = require('../src/utils/key-generator');
 const swaggerDocument = require('../openapi/openapi.json');
-jestOpenAPI(swaggerDocument);
 
+jestOpenAPI(swaggerDocument);
 const TEST_PUB_KEY = Generator.generateKeyPair().publicKey;
 const TEST_PUB_KEY2 = Generator.generateKeyPair().publicKey;
+const { HTTP_PORT } = require('../config/ports-folders');
+
 describe('GET /', () => {
     it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get('http://localhost:1337/');
-      expect(res.status).toEqual(200);
-      expect(res.data).toEqual("Welcome to AABRDP-simple-blockchain!");
-      
-    });
-  });
+        const res = await axios.get(`http://localhost:${HTTP_PORT}`);
 
-  describe('GET /chain', () => {
-    it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get('http://localhost:1337/chain');
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
+        expect(res.status).toEqual(200);
+        expect(res.data).toEqual('Welcome to AABRDP-simple-blockchain!');
     });
-  });
+});
 
-  describe('GET /generate', () => {
+describe('GET /chain', () => {
     it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get('http://localhost:1337/generate');
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-   
-    });
-  });
-  describe('GET /blocks/latest', () => {
-    it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get('http://localhost:1337/blocks/latest');
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-    });
-  });
-  describe('GET /transactions', () => {
-    it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get('http://localhost:1337/transactions');
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-    });
-  });
-  describe('GET /address/balance/:address', () => {
-    it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.get(`http://localhost:1337/address/balance/${TEST_PUB_KEY}`);
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-    });
-  });
-  describe('POST /addtransaction', () => {
-    it('should satisfy OpenAPI spec', async () => {
-       
-      
-      const res = await axios.post('http://localhost:1337/addtransaction',{fromAddress:TEST_PUB_KEY,toAddress:TEST_PUB_KEY2,amount:5});
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-    });
-  });
-  describe('POST /minetransactions', () => {
-    it('should satisfy OpenAPI spec', async () => {
-      
-      const res = await axios.post('http://localhost:1337/minetransactions',{mineraddress:TEST_PUB_KEY});
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
-    });
-  });
+        const res = await axios.get(`http://localhost:${HTTP_PORT}/chain`);
 
-  describe('POST /getcoins', () => {
-    it('should satisfy OpenAPI spec', async () => {
-     
-      
-      const res = await axios.post('http://localhost:1337/getcoins',{toAddress:TEST_PUB_KEY2});
-      expect(res.status).toEqual(200);
-      expect(res).toSatisfyApiSpec();
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
     });
-  });
+});
 
-  
+describe('GET /generate', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get(`http://localhost:${HTTP_PORT}/generate`);
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+describe('GET /blocks/latest', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get(`http://localhost:${HTTP_PORT}/blocks/latest`);
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+describe('GET /transactions', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get(`http://localhost:${HTTP_PORT}/transactions`);
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+describe('GET /address/balance/:address', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.get(`http://localhost:${HTTP_PORT}/address/balance/${TEST_PUB_KEY}`);
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+describe('POST /addtransaction', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.post(`http://localhost:${HTTP_PORT}/addtransaction`, { fromAddress: TEST_PUB_KEY, toAddress: TEST_PUB_KEY2, amount: 5 });
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+describe('POST /minetransactions', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.post(`http://localhost:${HTTP_PORT}/minetransactions`, { mineraddress: TEST_PUB_KEY });
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
+
+describe('POST /getcoins', () => {
+    it('should satisfy OpenAPI spec', async () => {
+        const res = await axios.post(`http://localhost:${HTTP_PORT}/getcoins`, { toAddress: TEST_PUB_KEY2 });
+
+        expect(res.status).toEqual(200);
+        expect(res).toSatisfyApiSpec();
+    });
+});
