8283ba58b745a4b3df4220b1e3526be30371fdb4
cr0c0pk1 <alpics1337@gmail.com>
2021-12-15 15:08:24 +0200

    feat: Add initial localhost peer

--- config/ports-folders.js
+++ config/ports-folders.js
@@ -1,7 +1,7 @@
 const KEY_PAIRS_FOLDER = 'keypairs';
 const HTTP_PORT = 1337;
 const WS_PORT = 9991;
-const PEERS = [];
+const PEERS = ['ws://localhost:9991'];
 
 module.exports = {
     KEY_PAIRS_FOLDER,
