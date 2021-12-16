6552541b81dbfe1c7a0613212cbb5c62679bb1a7
cr0c0pk1 <alpics1337@gmail.com>
2021-12-16 10:00:21 +0200

    fix: Add package-lock file

--- src/ws/server.js
+++ src/ws/server.js
@@ -83,7 +83,7 @@ class WsServer {
                 const socket = new Websocket(peer);
 
                 socket.on('open', () => this.connectSocket(socket));
-                socket.on('error', (error) => { console.log('Error: ' + error); });
+                socket.on('error', (error) => { console.log(`Error: ${error}`); });
             }
         });
     }
