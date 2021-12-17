bb5018187ff88c9677850460f151234a62758f7d
cr0c0pk1 <alpics1337@gmail.com>
2021-12-17 09:57:01 +0200

    update: Add new endpoints to openapi

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
         '@commitlint/config-conventional',
     ],
     rules: {
-        'type-enum': [2, 'always', ['docs', 'fix', 'feat', 'refactor', 'style', 'build', 'chore', 'revert', 'test']],
+        'type-enum': [2, 'always', ['docs', 'fix', 'feat', 'refactor', 'style', 'build', 'chore', 'revert', 'test', 'update']],
         'subject-case': [2, 'always', ['sentence-case']],
         'scope-empty': [2, 'always'],
     },
